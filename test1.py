2c0dbada2dfc705c6f0a072866ee69588551f531
meztim <mezhonovtim@gmail.com>
2025-05-18 17:27:20 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -39,23 +39,32 @@ class Platform2(Picture):
             self.rect.y += self.speed
 
 class Boll(Picture):
-    def __init__(self, image, w, h, x, y, speed):
+    def __init__(self, image, w, h, x, y, speed, speedx, speedy):
         super().__init__(image, w, h, x, y, speed)
-        self.speed_x = self.speed
-        self.speed_y = self.speed
+        self.speedx = speedx
+        self.speedy = speedy
 
-    def colliderect(self,rect):
-        if self.rect.colliderect(rect):
-            self.speed_y *= -1
-        elif self.x > 445:
-            self.x = 445
-            self.speed_x *= -1
-        elif self.y < 0 :
-            self.y = 0
-            self.speed_y *= -1
-        elif self.x < 0:
-            self.x = 0
-            self.speed_x *= -1
+    def update(self):
+        self.rect.x -= self.speedx
+        self.rect.y -= self.speedy
+        if self.rect.colliderect(platform1.rect):
+            self.speedx = -self.speedx
+        elif self.rect.y == 0  :
+            self.speedy = -self.speedy
+        elif self.rect.colliderect(platform2.rect):
+            self.speedx = -self.speedx
+        elif self.rect.y == 435:
+            self.speedy = -self.speedy
+
+    def recet (self):
+        if boll.rect.x >= 0:
+            boll.rect.x = 285
+            boll.rect.y = 185
+            a += 1
+        elif boll.rect.x >= 700:
+            boll.rect.x = 285
+            boll.rect.y = 185
+            b += 1
 
 
 
@@ -64,14 +73,31 @@ display.set_caption('Пинг-понг')
 background = transform.scale(image.load('YU69WYqi1Vg.jpg'),(700,500))
 
 #создание объктов игры
-boll = Boll('wimbledon.crop_642x481_0,7.preview.jpg', 65, 65, 330, 180, 5 )
-platform1 = Platform1('8a3e58af9aebd6aad8c51f6c9d8fdfcd.jpg', 30, 100, 40, 150, 5 )
-platform2 = Platform2('d8e9f9afeae4571b6d3427115de34f45.jpg', 30, 100, 660, 150, 5)
+boll = Boll('wimbledon.crop_642x481_0,7.preview.jpg', 65, 65, 330, 180, 5, 3, 1 )
+platform1 = Platform1('8a3e58af9aebd6aad8c51f6c9d8fdfcd.jpg', 30, 100, 40, 150, 6)
+platform2 = Platform2('d8e9f9afeae4571b6d3427115de34f45.jpg', 30, 100, 660, 150, 6)
 #Настрока частоты обновления экрана
 clock = time.Clock()
 FPS = 60
 
+#текс
+font.init()
+my_font = font.SysFont(None, 80)
+text_ska = my_font.render('скала выйграла', True, (78, 194, 54))
+text_gor = my_font.render('гора выйграла', True, (117, 0, 0))
+
+
+#текс игры
+font.init()
+font = font.SysFont(None, 30)
+text1 = 'Счёт:1'
+text2 = 'Cчет:'
+
+
 code = 0
+
+a = 0
+b = 0
 
 game = True
 while game:
@@ -79,18 +105,35 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if code == 0:
         keys = key.get_pressed()
         platform1.move(keys)
         platform2.move(keys)
 
-    #отрисовка
-    window.blit(background,(0,0))
-    boll.reset()
-    boll.colliderect(platform1.rect)
-    platform1.reset()
-    platform2.reset()
+        #отрисовка
+        window.blit(background,(0,0))
+        boll.reset()
+        boll.update()
+        platform1.reset()
+        platform2.reset()
+
+            surface = font.render(text2 + str(b), True, (0, 255, 255))
+            window.blit(surface, (30, 30))
+
+            surface1 = font.render(text1 + str(a), True, (0, 255, 255))
+            window.blit(surface1, (610, 30))
+
+        if a >= 3:
+            code = 1
+        if b >= 3:
+            code = 2
+
+    if code == 1:
+        window.blit(text_gor,(145,225))
+    if code == 2 :
+        window.blit(text_ska, (145, 225))
+
 
     #Обновление экрана
     display.update()
     clock.tick(FPS)
-
